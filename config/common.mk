2a17cfe296db71d430543fc8b2a841c9ae7de899
siracuervo <fl.siracusa@outlook.com.ar>
2014-04-10 14:55:40 -0300

    Revert "Remove selinux make and change default notif ogg"
    This reverts commit 1cd248a1bacf0e3f1e779740f290fd9889331c0b.

--- config/common.mk
+++ config/common.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     vendor/axxion/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit
 
+# SELinux filesystem labels
+PRODUCT_COPY_FILES += \
+    vendor/axxion/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
+
 # axxion-specific init file
 PRODUCT_COPY_FILES += \
     vendor/axxion/prebuilt/common/etc/init.local.rc:root/init.axxion.rc
