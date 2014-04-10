0de5acd8346d283c0a0f9b5afc5c5b56ece4c191
Sira Cuervo <fl.siracusa@outlook.com.ar>
2014-04-10 20:15:21 -0300

    Fix ROOT¿?

--- config/common.mk
+++ config/common.mk
@@ -71,7 +71,7 @@ PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
 
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 # Enable ADB authentication
-ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=1
+ADDITIONAL_DEFAULT_PROPERTIES += ro.adb.secure=3
 endif
 
 # Copy over the changelog to the device
